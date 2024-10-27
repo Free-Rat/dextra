5d4cb31a43457722c75e80532b36c7505d599618
Free-Rat <lawicki02@gmail.com>
2024-10-27 14:14:32 +0100

    swift and c interface

---
+++ flake.nix
@@ -0,0 +1,33 @@
+# flake.nix
+{
+	outputs = { self, nixpkgs, flake-utils, }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let
+				pkgs = nixpkgs.legacyPackages.${system};
+
+				lib-path = with pkgs; lib.makeLibraryPath [
+					swift
+					swiftpm
+					swiftPackages.Foundation
+					swift-corelibs-libdispatch
+				];
+			in
+				{
+				devShells.default = pkgs.mkShell.override { stdenv = pkgs.swift.stdenv; } {
+					buildInputs = with pkgs;[
+						swift
+						swiftpm
+						swiftPackages.Foundation
+						# darwin.apple_sdk.frameworks.AppKit
+					];
+
+					shellHook = ''
+			export PS1='\n\[\033[1;32m\][nix-shell:\w]\$\[\033[0m\] '
+
+			export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+			export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${lib-path}
+
+					'';
+				};
+			});
+}
