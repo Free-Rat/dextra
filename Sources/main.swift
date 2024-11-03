9a16f506744d3b6e40b14ecb4745b269dfb3b424
Free-Rat <lawicki02@gmail.com>
2024-11-03 18:57:27 +0100

    printing given in args file

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,37 +2,37 @@ import CShit
 import curses
 import Foundation
 
+func readFile(path: String) -> String? {
+    let contents: String? = try? String(contentsOfFile: path, encoding: .utf8)
+    return contents
+}
+
 func main() {
+
+    if CommandLine.arguments.count < 2 {
+        print("no file path provided")
+        exit(1)
+    }
+    guard let path = ProcessInfo.processInfo.environment["PWD"] else {
+        print("cant found PWD environment variable")
+        exit(2)
+    }
+    let fileName = CommandLine.arguments[1]
+
     // Initialize ncurses
     initscr()                // Start ncurses mode
     cbreak()                 // Disable line buffering
     noecho()                 // Don’t echo input characters
     keypad(stdscr, true)     // Enable arrow keys and function keys
 
-    // Print a message at a specific location
-    let message = "Hello, ncurses from Swift!"
-    // mvprintw(10, 10, message)
-    mvaddstr(10, 10, message)
-
-    refresh()
-
-    for x in 0...10 {
-        for y in 0...10 {
-            let x = Int32(x)
-            let y = Int32(y)
-            mvaddstr(x, y, "X")
-            refresh()
-        }
-    }
-
-    mvaddstr(10, 40, "\(makeResult())")
+    mvaddstr(0, 0, "\(path)")
+    // mvaddstr(0, 30, "\(makeResult())")
+    mvaddstr(1, 0, readFile(path: "\(path)/\(fileName)"))
 
     // Refresh the screen to show changes
     refresh()
-
     // Wait for the user to press any key
     getch()
-
     // Clean up
     endwin()                 // End ncurses mode
 }
