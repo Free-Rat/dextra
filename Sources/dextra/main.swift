cf433d1caecb9988530330a37e1947965f12c5fc
Free-Rat <lawicki02@gmail.com>
2025-02-10 02:32:10 +0100

    moving cursor

--- Sources/dextra/main.swift
+++ Sources/dextra/main.swift
@@ -11,7 +11,6 @@ func readFile(path: String) -> String? {
 }
 
 func main() {
-
     if CommandLine.arguments.count < 2 {
         print("no file path provided")
         exit(1)
@@ -28,27 +27,64 @@ func main() {
     noecho()                 // Don’t echo input characters
     keypad(stdscr, true)     // Enable arrow keys and function keys
 
-    mvaddstr(0, 0, "\(path)")
-    // mvaddstr(0, 0, "\(some_func())")
-    mvaddstr(0, 0, "\(hello_func())")
+    // mvaddstr(0, 0, "\(path)")
+    // mvaddstr(0, 0, "\(hello_func())")
 
     // mvaddstr(0, 30, "\(makeResult())")
-    mvaddstr(1, 0, readFile(path: "\(path)/\(fileName)"))
+    // mvaddstr(0, 0, readFile(path: "\(path)/\(fileName)"))
+    let fileContent = readFile(path: "\(path)/\(fileName)") ?? "Failed to read file"
+    mvaddstr(0, 0, fileContent)
 
-    // Refresh the screen to show changes
     refresh()
-    // Wait for the user to press any key
+    var cursorX: Int32 = 0
+    var cursorY: Int32 = 0
+    move(cursorY, cursorX)
+
     while true {
         let char = getch()
-        print(char)
-        let quitChar: Character = "q"
-        if char == quitChar.asciiValue! {
+
+        switch char {
+        case 113: // ASCII for 'q'
+            break
+        case KEY_LEFT:
+            if cursorX > 0 {
+                cursorX -= 1
+            }
+        case KEY_RIGHT:
+            if cursorX < fileContent.count - 1 {
+                cursorX += 1
+            }
+        case KEY_UP:
+            if cursorY > 0 {
+                cursorY -= 1
+            }
+        case KEY_DOWN:
+            cursorY += 1
+
+        default:
+            refresh()
+
+        }
+
+        move(cursorY, cursorX)
+        refresh()
+
+        if char == 113 { // 'q' key to quit
             break
         }
+
     }
+    // while true {
+    //     let char = getch()
+    //     print(char)
+    //     let quitChar: Character = "q"
+    //     if char == quitChar.asciiValue! {
+    //         break
+    //     }
+    //     refresh()
+    // }
     // Clean up
     endwin()
-
 }
 
 main()
--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
-cc -c Sources/CShit/test.c -shared -o Sources/CShit/libtest.so
+# cc -c Sources/CShit/test.c -shared -o Sources/CShit/libtest.so
+# swiftc Sources/main.swift -I Sources -L Sources/CShit -L Sources/curses  -ltest
 
-swiftc Sources/main.swift -I Sources -L Sources/CShit -L Sources/curses  -ltest
+swift build -c release
+cp .build/release/dextra ./main 
 
 # ./main
